421027c52437c35a5ddee87a3dcff39a44caf586
kristen <1065200748@qq.com>
2023-05-11 16:16:03 +0800

    2023.5.11

--- src/main/java/com/learn/chapter1/MainApp.java
+++ src/main/java/com/learn/chapter1/MainApp.java
@@ -1,47 +1,46 @@
 package com.learn.chapter1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MainApp {
-   // JDBC driver name and database URL
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost:3366/emp";
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3366/emp";
 
-   //  Database credentials
-   static final String USER = "root";
-   static final String PASS = "";
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "";
 
-   public static void main(String[] args) throws SQLException {
-      Connection conn = null;
-      QueryRunner queryRunner = new QueryRunner();
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        QueryRunner queryRunner = new QueryRunner();
 
-      //Step 1: Register JDBC driver
-      DbUtils.loadDriver(JDBC_DRIVER);
+        //Step 1: Register JDBC driver
+        DbUtils.loadDriver(JDBC_DRIVER);
 
-      //Step 2: Open a connection
-      System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        //Step 2: Open a connection
+        System.out.println("Connecting to database...");
 
-      //Step 3: Create a ResultSet Handler to handle Employee Beans
-      ResultSetHandler<Employee> resultHandler = new BeanHandler<Employee>(Employee.class);
+        //Step 3: Create a ResultSet Handler to handle Employee Beans
+        ResultSetHandler<Employee> resultHandler = new BeanHandler<Employee>(Employee.class);
 
-      try {
-         Employee emp = queryRunner.query(conn, "SELECT * FROM employees WHERE first=?", resultHandler, "Sumit");
-         //Display values
-         System.out.print("ID: " + emp.getId());
-         System.out.print(", Age: " + emp.getAge());
-         System.out.print(", First: " + emp.getFirst());
-         System.out.println(", Last: " + emp.getLast());
-      } finally {
-         DbUtils.close(conn);
-      }              
-   }
+        try {
+            Employee emp = queryRunner.query(conn, "SELECT * FROM employees WHERE first=?", resultHandler, "Sumit");
+            //Display values
+            System.out.print("ID: " + emp.getId());
+            System.out.print(", Age: " + emp.getAge());
+            System.out.print(", First: " + emp.getFirst());
+            System.out.println(", Last: " + emp.getLast());
+        } finally {
+            DbUtils.close(conn);
+        }
+    }
 }
 
--- src/main/java/com/learn/chapter2/MainApp.java
+++ src/main/java/com/learn/chapter2/MainApp.java
@@ -1,30 +1,27 @@
 package com.learn.chapter2;
 
+import org.apache.commons.dbutils.DbUtils;
+import org.apache.commons.dbutils.QueryRunner;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.apache.commons.dbutils.DbUtils;
-import org.apache.commons.dbutils.QueryRunner;
-
 public class MainApp {
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/emp";
+    static final String DB_URL = "jdbc:mysql://localhost:3366/emp";
 
     //  Database credentials
     static final String USER = "root";
-    static final String PASS = "admin";
+    static final String PASS = "";
 
     public static void main(String[] args) throws SQLException {
-        Connection conn = null;
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
         QueryRunner queryRunner = new QueryRunner();
         DbUtils.loadDriver(JDBC_DRIVER);
-        conn = DriverManager.getConnection(DB_URL, USER, PASS);
         try {
-            int insertedRecords = queryRunner.update(conn,
-                    "INSERT INTO employees(id,age,first,last)  VALUES (?,?,?,?)",
-                    104, 30, "Sohan", "Kumar");
+            int insertedRecords = queryRunner.update(conn, "INSERT INTO employees(id,age,first,last)  VALUES (?,?,?,?)", 104, 30, "Sohan", "Kumar");
             System.out.println(insertedRecords + " record(s) inserted");
         } finally {
             DbUtils.close(conn);
