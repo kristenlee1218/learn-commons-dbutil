dd0c6c849ae726ee1133627772703a76eccd0340
kristen <1065200748@qq.com>
2023-05-12 13:36:03 +0800

    2023.5.12

--- src/main/java/com/learn/chapter12/MainApp.java
+++ src/main/java/com/learn/chapter12/MainApp.java
@@ -1,19 +1,16 @@
-package com.learn.chapter11;
+package com.learn.chapter12;
 
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.MapListHandler;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author ：Kristen
  * @date ：2023/5/12
- * @description : MapListHandler 类
+ * @description : DBUtils 自定义处理程序
  */
 
 public class MainApp {
@@ -29,11 +26,16 @@ public class MainApp {
         Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
         QueryRunner queryRunner = new QueryRunner();
         DbUtils.loadDriver(JDBC_DRIVER);
-        System.out.println("Connecting to database...");
+        EmployeeHandler employeeHandler = new EmployeeHandler();
+
         try {
-            List<Map<String, Object>> result
-                    = queryRunner.query(conn, "SELECT * FROM employees", new MapListHandler());
-            System.out.println(result);
+            Employee emp = queryRunner.query(conn, "SELECT * FROM employees WHERE first=?",
+                    employeeHandler, "Sumit");
+
+            //Display values
+            System.out.print("ID: " + emp.getId());
+            System.out.print(", Age: " + emp.getAge());
+            System.out.print(", Name: " + emp.getName());
         } finally {
             DbUtils.close(conn);
         }
---
+++ src/main/java/com/learn/chapter14/CustomDataSource.java
@@ -0,0 +1,35 @@
+package com.learn.chapter14;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * @author ：Kristen
+ * @date ：2023/5/12
+ * @description :
+ */
+
+public class CustomDataSource {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3366/emp";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "";
+    private static final BasicDataSource basicDataSource;
+
+    static {
+        basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName(JDBC_DRIVER);
+        basicDataSource.setUsername(USER);
+        basicDataSource.setPassword(PASS);
+        basicDataSource.setUrl(DB_URL);
+    }
+
+    public static DataSource getInstance() {
+        return basicDataSource;
+    }
+}
+
